cab84ef1ec5db72574026b1de3008d12a408a539
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2025-09-14 20:59:49 -0500

    chore(deps): update dependencies, reconfigure source tree
    - update to nuxt 4
    - update various dependencies
    - locally install icon libraries
    - install code formatting tools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,36 +1,40 @@
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineNuxtConfig({
-    ssr: true,
-    compatibilityDate: '2024-11-01',
-    devtools: { enabled: true },
-    srcDir: 'src',
-    css: ['~/assets/css/main.css'],
-    vite: {
-        plugins: [
-            tailwindcss()
-        ],
-    },
-    nitro: {
-        preset: 'static',
-        prerender: {
-            crawlLinks: true,
-            routes: ['/']
-        }
-    },
+	ssr: true,
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
+	srcDir: 'src',
+	css: ['~/assets/css/main.css'],
+	vite: {
+		plugins: [tailwindcss()]
+	},
+	nitro: {
+		preset: 'static',
+		prerender: {
+			crawlLinks: true,
+			routes: ['/']
+		}
+	},
 
-    modules: [
-        ['@nuxtjs/google-fonts', {
-            families: {
-                'Noto+Sans': true
-            }
-        }], 
-        ['@nuxt/icon', {
-            icon: {
-                mode: 'css',
-                cssLayer: 'base',
-                size: '48px'
-            }
-        }]
-    ],
-})
+	modules: [
+		[
+			'@nuxtjs/google-fonts',
+			{
+				families: {
+					'Noto+Sans': true
+				}
+			}
+		],
+		[
+			'@nuxt/icon',
+			{
+				icon: {
+					mode: 'css',
+					cssLayer: 'base',
+					size: '48px'
+				}
+			}
+		]
+	]
+});
